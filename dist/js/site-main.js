ab22eeabdd9c6020cd872e3fb99178333fe97aaf
Moshkin Denis <disant1998@yandex.ru>
2021-04-20 22:31:05 +0300

    add skills section

--- dist/js/site-main.js
+++ dist/js/site-main.js
@@ -847,3 +847,13 @@ $(document).ready(function () {
 
 });
 
+
+$(document).ready(function () {
+	/* ------------- scroll ------------ */
+
+	$("a[href^='#']").click(function () {
+		var _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+		return false;
+	});
+});
